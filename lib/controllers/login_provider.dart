6f82e97933d26a49aa22d36ceaa7330381b7f1ca
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-09-19 11:14:17 +0600

    update login provider.

--- lib/controllers/login_provider.dart
+++ lib/controllers/login_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginNotifier extends ChangeNotifier{
   bool _obsecureText = true;
@@ -8,5 +9,20 @@ class LoginNotifier extends ChangeNotifier{
   set obsecureText(bool newState) {
     _obsecureText = newState;
     notifyListeners();  // for update state
+  }
+
+  bool? _loggedIn;
+
+  bool get loggedIn => _loggedIn??false; // null thake false return korbe
+
+  set loggedIn(bool newState) {
+    _loggedIn = newState;
+    notifyListeners();  // for update state
+  }
+
+  getPrefs()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    loggedIn = prefs.getBool('loggedIn')??false;
+    //entrypoint = prefs.getBool('entrypoint')??false;
   }
 }
