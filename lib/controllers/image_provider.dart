5881e653f021095aa7d7a5da8011b2e45b952de2
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-09-28 16:26:49 +0600

    firebase stroage and uuid dependency added. also complete function which is store image in firebase stroage. be careful when complete this part application close and again install otherwise not work show error.

--- lib/controllers/image_provider.dart
+++ lib/controllers/image_provider.dart
@@ -1,9 +1,13 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
-class ImageUploader extends ChangeNotifier{
+class ImageUploader extends ChangeNotifier {
+  var uuid = const Uuid();
   final ImagePicker _picker = ImagePicker();
 
   String? imageUrl;
@@ -11,55 +15,61 @@ class ImageUploader extends ChangeNotifier{
 
   List<String> imageFil = [];
 
-  void pickImage()async{
+  void pickImage() async {
     XFile? _imageFile = await _picker.pickImage(source: ImageSource.gallery);
 
     _imageFile = await cropImage(_imageFile!);
 
-
-    if(_imageFile != null)
-      {
-        imageFil.add(_imageFile.path);
-        imagePath = _imageFile.path;
-      }
-    else
-      {
-        return;
-      }
-
-  }
-  Future<XFile?> cropImage(XFile imageFile)async{
-CroppedFile? croppedFile = await ImageCropper.platform.cropImage(
-    sourcePath: imageFile.path,
-  maxHeight: 800,
-  maxWidth: 600,
-  compressQuality: 70,
-  // aspectRatio: [CropAspectRatioPreset.ratio5x4],
-  uiSettings: [
-    AndroidUiSettings(
-      toolbarTitle: 'JobHub Cropper',
-      toolbarColor: Colors.lightBlue,
-      toolbarWidgetColor: Colors.white,
-      initAspectRatio: CropAspectRatioPreset.ratio5x4,
-      cropStyle: CropStyle.rectangle,
-      lockAspectRatio: true
-    ),
-    IOSUiSettings(
-      title: 'JobHub Cropper',
-      cropStyle: CropStyle.rectangle
-    )
-  ]
-);
-
-if(croppedFile != null)
-  {
-    notifyListeners();
-    return XFile(croppedFile.path);
-  }
-else{
-  return null;
-}
+    if (_imageFile != null) {
+      imageFil.add(_imageFile.path);
+      // ai function upload function ke call korbe image store korar jonno
+      imageUpload(_imageFile);
+      imagePath = _imageFile.path;
+    } else {
+      return;
+    }
   }
 
+  Future<XFile?> cropImage(XFile imageFile) async {
+    CroppedFile? croppedFile = await ImageCropper.platform.cropImage(
+        sourcePath: imageFile.path,
+        maxHeight: 800,
+        maxWidth: 600,
+        compressQuality: 70,
+        // aspectRatio: [CropAspectRatioPreset.ratio5x4],
+        uiSettings: [
+          AndroidUiSettings(
+              toolbarTitle: 'JobHub Cropper',
+              toolbarColor: Colors.lightBlue,
+              toolbarWidgetColor: Colors.white,
+              initAspectRatio: CropAspectRatioPreset.ratio5x4,
+              cropStyle: CropStyle.rectangle,
+              lockAspectRatio: true),
+          IOSUiSettings(title: 'JobHub Cropper', cropStyle: CropStyle.rectangle)
+        ]);
 
+    if (croppedFile != null) {
+      notifyListeners();
+      return XFile(croppedFile.path);
+    } else {
+      return null;
+    }
+  }
+
+  // ai function firebase stroage a picture store korbe
+  Future<String?> imageUpload(XFile upload) async {
+    File image = File(upload.path);
+
+    final ref = FirebaseStorage.instance
+        .ref()
+        .child('jobhub')
+        .child('${uuid.v1()}.jpg');
+    // image ke store kora hosse
+    await ref.putFile(image);
+
+    imageUrl = (await ref.getDownloadURL());
+    print(imageUrl);
+
+    return imageUrl;
+  }
 }
