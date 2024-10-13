a1b4714dd17e83b65b1d732b5bc576841b5fed10
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-10-13 17:20:07 +0600

    add padding in bookmark page.

--- lib/views/ui/BookMarkPage.dart
+++ lib/views/ui/BookMarkPage.dart
@@ -24,49 +24,65 @@ class BookMarkPage extends StatelessWidget {
         preferredSize: Size.fromHeight(50),
         child: CustomAppbar(
           text: 'BookMarks',
-
           child: Padding(
             padding: const EdgeInsets.all(12.0),
             child: DrawerWidget(),
-          ),),
+          ),
+        ),
       ),
       body: Consumer<BookMarkNotifier>(
         builder: (context, bookmarkNotifier, child) {
-          bookmarkNotifier.getBookMarks(); // at first get all bookmarks from server
-        return FutureBuilder(
-          // jobList take access kore neya hosse
-          future: bookmarkNotifier.bookmarks,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState ==
-                ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error is: ${snapshot.error}'),
-              );
-            } else {
-              var bookmarks = snapshot.data;
+          bookmarkNotifier
+              .getBookMarks(); // at first get all bookmarks from server
+          return FutureBuilder(
+            // jobList take access kore neya hosse
+            future: bookmarkNotifier.bookmarks,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error is: ${snapshot.error}'),
+                );
+              } else {
+                var bookmarks = snapshot.data;
 // be careful return otherwise not find what happend error. because not use return does not show any info
-              return  bookmarks!.isNotEmpty ?
-              ListView.separated(
-                  itemCount: bookmarks.length, // total bookmarklist length
-                separatorBuilder: (context, index) {
-                  return SizedBox(height: 10,);
-                },
-                  // scrollDirection: Axis.horizontal,
+                return bookmarks!.isNotEmpty
+                    ? ListView.separated(
+                        itemCount:
+                            bookmarks.length, // total bookmarklist length
+                        separatorBuilder: (context, index) {
+                          return SizedBox(
+                            height: 10,
+                          );
+                        },
+                        // scrollDirection: Axis.horizontal,
 
-                  itemBuilder: (context, index) {
-                    var bookmark = bookmarks[index];  // single bookmark from list
-return VerticalTile(job: bookmark.job, onTap: (){
-  Get.to(JobPage(title: bookmark.job.company, id: bookmark.job.id));
-},);
-                  }): Center(child: Text("You haven't already created any bookmark"));
-            }
-          },
-        );
-      },),
+                        itemBuilder: (context, index) {
+                          var bookmark =
+                              bookmarks[index]; // single bookmark from list
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: VerticalTile(
+                              job: bookmark.job,
+                              onTap: () {
+                                Get.to(JobPage(
+                                    title: bookmark.job.company,
+                                    id: bookmark.job.id));
+                              },
+                            ),
+                          );
+                        })
+                    : Center(
+                        child:
+                            Text("You haven't already created any bookmark"));
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
