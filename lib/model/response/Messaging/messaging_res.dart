4c5805365bcb5828fcc9262cfcf7d1f48c986cba
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-10-16 09:33:22 +0600

    add get messages function and complete messaging helper.

--- lib/model/response/Messaging/messaging_res.dart
+++ lib/model/response/Messaging/messaging_res.dart
@@ -2,96 +2,130 @@
 //
 //     final receivedMessage = receivedMessageFromJson(jsonString);
 
-import 'package:meta/meta.dart';
 import 'dart:convert';
-// i remove all toJson() because no need for message receive response
-ReceivedMessage receivedMessageFromJson(String str) => ReceivedMessage.fromJson(json.decode(str));
+
+List<ReceivedMessages> receivedMessagesFromJson(String str) => List<ReceivedMessages>.from(json.decode(str).map((x) => ReceivedMessages.fromJson(x)));
 
 
-class ReceivedMessage {
+class ReceivedMessages {
   final String id;
   final Sender sender;
   final String content;
-  final String receiver;
-  final Chat chat;
-  final List<dynamic> readBy;
-  final DateTime createdAt;
-  final DateTime updatedAt;
-  final int v;
+  final String? receiver;
+  final Chat? chat;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
 
-  ReceivedMessage({
+  ReceivedMessages({
     required this.id,
     required this.sender,
     required this.content,
-    required this.receiver,
-    required this.chat,
-    required this.readBy,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.v,
+    this.receiver,
+    this.chat,
+    this.createdAt,
+    this.updatedAt,
   });
 
-  factory ReceivedMessage.fromJson(Map<String, dynamic> json) => ReceivedMessage(
+  factory ReceivedMessages.fromJson(Map<String, dynamic> json) => ReceivedMessages(
     id: json["_id"],
     sender: Sender.fromJson(json["sender"]),
     content: json["content"],
     receiver: json["receiver"],
-    chat: Chat.fromJson(json["chat"]),
-    readBy: List<dynamic>.from(json["readBy"].map((x) => x)),
-    createdAt: DateTime.parse(json["createdAt"]),
-    updatedAt: DateTime.parse(json["updatedAt"]),
-    v: json["__v"],
+    chat: json["chat"] == null ? null : Chat.fromJson(json["chat"]),
+    createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
+    updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
   );
 
-
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "sender": sender.toJson(),
+    "content": content,
+    "receiver": receiver,
+    "chat": chat?.toJson(),
+    "createdAt": createdAt?.toIso8601String(),
+    "updatedAt": updatedAt?.toIso8601String(),
+  };
 }
 
 class Chat {
-  final String id;
-  final String chatName;
-  final bool isGroupChat;
-  final List<Sender> users;
-  final DateTime createdAt;
-  final DateTime updatedAt;
-  final int v;
-  final String latestMessage;
+  final String? id;
+  final String? chatName;
+  final bool? isGroup;
+  final List<User>? users;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final String? latestMessage;
 
   Chat({
-    required this.id,
-    required this.chatName,
-    required this.isGroupChat,
-    required this.users,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.v,
-    required this.latestMessage,
+    this.id,
+    this.chatName,
+    this.isGroup,
+    this.users,
+    this.createdAt,
+    this.updatedAt,
+    this.latestMessage,
   });
 
   factory Chat.fromJson(Map<String, dynamic> json) => Chat(
     id: json["_id"],
     chatName: json["chatName"],
-    isGroupChat: json["isGroupChat"],
-    users: List<Sender>.from(json["users"].map((x) => Sender.fromJson(x))),
-    createdAt: DateTime.parse(json["createdAt"]),
-    updatedAt: DateTime.parse(json["updatedAt"]),
-    v: json["__v"],
+    isGroup: json["isGroup"],
+    users: json["users"] == null ? [] : List<User>.from(json["users"]!.map((x) => User.fromJson(x))),
+    createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
+    updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
     latestMessage: json["latestMessage"],
   );
 
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "chatName": chatName,
+    "isGroup": isGroup,
+    "users": users == null ? [] : List<dynamic>.from(users!.map((x) => x.toJson())),
+    "createdAt": createdAt?.toIso8601String(),
+    "updatedAt": updatedAt?.toIso8601String(),
+    "latestMessage": latestMessage,
+  };
+}
 
+class User {
+  final String? id;
+  final String? username;
+  final String? email;
+  final String? profile;
+
+  User({
+    this.id,
+    this.username,
+    this.email,
+    this.profile,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    id: json["_id"],
+    username: json["username"],
+    email: json["email"],
+    profile: json["profile"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "username": username,
+    "email": email,
+    "profile": profile,
+  };
 }
 
 class Sender {
   final String id;
-  final String username;
-  final String email;
-  final String profile;
+  final String? username;
+  final String? email;
+  final String? profile;
 
   Sender({
     required this.id,
-    required this.username,
-    required this.email,
-    required this.profile,
+    this.username,
+    this.email,
+    this.profile,
   });
 
   factory Sender.fromJson(Map<String, dynamic> json) => Sender(
@@ -101,5 +135,12 @@ class Sender {
     profile: json["profile"],
   );
 
-
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "username": username,
+    "email": email,
+    "profile": profile,
+  };
 }
+
+
--- lib/services/helper/messaging_helper.dart
+++ lib/services/helper/messaging_helper.dart
@@ -9,7 +9,7 @@ import '../config.dart';
 
 class MessagingHelper{
 // Send a message
-  static Future<List<dynamic>> apply(SendMessage model)async{
+  static Future<List<dynamic>> sendMessage(SendMessage model)async{
     final SharedPreferences pref = await SharedPreferences.getInstance();
     String? token = pref.getString("token");
 
@@ -34,8 +34,8 @@ class MessagingHelper{
 
     if(response!.statusCode == 200)
     {
-      ReceivedMessage receivedMessage = receivedMessageFromJson(response.body);
-      print('received message is: $receivedMessage');
+      ReceivedMessages receivedMessage = ReceivedMessages.fromJson(jsonDecode(response.body));
+      //print('received message is: $receivedMessage');
 
       Map<String, dynamic> responseMap = jsonDecode(response.body);
 
@@ -43,6 +43,42 @@ class MessagingHelper{
     }
     else{
       return [false];
+    }
+
+  }
+
+  // Get messages
+  static Future<List<ReceivedMessages>> getMessages(String chatId, int offset)async{
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    String? token = pref.getString("token");
+
+    // String? userId = pref.getString('userId');
+
+    http.Response? response;
+    Map<String, String> requestHeaders = {
+      "Content-Type": "application/json",
+      "x-auth-token": '$token'  // this is x-auth-token same as backend req.header("x-auth-token");
+    };
+    try{
+      response = await http.get(
+          Uri.parse('${Config.apiUrl}${Config.messagingUrl}/$chatId'),
+          headers: requestHeaders,
+      )
+      ;
+    }catch(e)
+    {
+      print('BookMark Add api call error: $e');
+    }
+
+    if(response!.statusCode == 200)
+    {
+      List<ReceivedMessages> messages = receivedMessagesFromJson(response.body);
+
+      return messages;
+
+    }
+    else{
+      throw Exception('Failed load Message');
     }
 
   }
